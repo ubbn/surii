bf3caaa637fe72aea683e456da106daf1cc47b3f
ubbn <ubbn@live.com>
2023-12-10 18:40:15 +0100

    Containerize development environment

--- frontend/src/components/auth/firebase.ts
+++ frontend/src/components/auth/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoFu3yC0KXp93EhnRjefzAp6tGw0feixw",
-  authDomain: "whatisthat-a518c.firebaseapp.com",
-  projectId: "whatisthat-a518c",
-  storageBucket: "whatisthat-a518c.appspot.com",
-  messagingSenderId: "489603733778",
-  appId: "1:489603733778:web:1995dcb277db9183b0bbcb",
-  measurementId: "G-ELM1GSYXWT",
+  apiKey: "AIzaSyCknPCYNRuezYurhsrvYnvC-7_g2P6Zccw",
+  authDomain: "b-learn-57a09.firebaseapp.com",
+  projectId: "b-learn-57a09",
+  storageBucket: "b-learn-57a09.appspot.com",
+  messagingSenderId: "316265251528",
+  appId: "1:316265251528:web:66da54492cc205b6c85bfd",
+  measurementId: "G-6H53PKYXBE",
 };
 
 const app = initializeApp(firebaseConfig);
