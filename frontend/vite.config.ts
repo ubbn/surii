83e4e4add9652cc894ab3378ae4d3b9c42b4731c
ubbn <ubbn@live.com>
2023-12-16 11:21:41 +0100

    Upgrade antd to v5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        additionalData: "@root-entry-name: default;",
-      },
-    },
-  },
   resolve: {
     alias: [{ find: /^~/, replacement: "" }],
   },
